2691e0c9fc1d9b6ac6563771d86e70210cb8f5a4
Expl0ced <56272035+Expl0ced@users.noreply.github.com>
2022-07-13 15:37:31 -0400

    13/07

---
+++ ejercicio_1.py
@@ -0,0 +1,43 @@
+import numpy as np
+import random
+
+
+def leer():
+    numeros=[]
+    with open('numeros_prueba.txt','r') as file:
+        for linea in file:
+            numeros.append(int(linea))
+
+    print(numeros)
+
+def escribir():
+    for i in range(0,10):
+        numero=random.randint(100,1000)
+        with open('numeros_prueba.txt', 'a+', encoding='utf-8') as file:
+            file.write(str(numero))
+            file.write("\n")
+
+def impares():
+    impares=[]
+    with open('numeros_prueba.txt', 'r') as file:
+        linea=[linea.split() for linea in file]
+        # como para cada digito se creo un lista, uso un comodin para poder extraer cada uno de esos de su respectiva lista y asi que solo queden como texto y transformarlos en int
+        for comodin in linea:
+            for impar in comodin:
+                impar=int(impar)
+                if impar % 2 != 0:
+                    impares.append(impar)
+    print(impares)
+    impares=np.array(impares)
+    print('la dimension de la lista es: '+ str(impares.ndim))
+
+
+def main():
+    escribir()
+    leer()
+    impares()
+
+
+
+if __name__ == '__main__':
+    main()
